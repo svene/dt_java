b781b94334e1bcb6baf2ca1311296612bab2429c
Sven Ehrke <sven.ehrke@canoo.com>
2011-11-03 10:41:29 +0100

    using dt_java:0.0.5

--- gdt.groovy
+++ gdt.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='sven-github', root='http://svene.github.com/maven2/releases')
-@Grab(group='org.svenehrke', module='dt_java', version='0.0.4')
+@Grab(group='org.svenehrke', module='dt_java', version='0.0.5')
 import org.svenehrke.dt.Invoker
 
 new Invoker(args).run()
