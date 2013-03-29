1aa4962b980d15bfcb87fb37b31fd902de1b2312
Sven Ehrke <sven.ehrke@canoo.com>
2013-03-29 07:14:01 +0100

    config file for simplejava

---
+++ templatedirectory/simplejava/.config/dt_config.groovy
@@ -0,0 +1,16 @@
+def util = new org.svenehrke.directorytemplate.DTUtil()
+
+config {
+	parameters = [
+		[name: 'packagename', value: 'org.svenehrke.simplejava', prompt: 'Packagename'],
+	]
+	transformer = { params ->
+		def result = []
+		result.addAll(params)
+
+		def p = params.find {param -> param.name == 'packagename'}
+		result << [name: '@packagename@', value: util.dotsToSlashes(p.value), prompt: p.prompt]
+
+		result
+	}
+}
