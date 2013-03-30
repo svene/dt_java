1146a5bc2cefc3343ae41019e2da4d48f1cfd4cf
Sven Ehrke <sven.ehrke@canoo.com>
2013-03-30 17:17:04 +0100

    removed code part from template project

--- gdt.groovy
+++
@@ -1,5 +0,0 @@
-@GrabResolver(name='sven-github', root='http://svene.github.com/maven2/releases')
-@Grab(group='org.svenehrke', module='dt_java', version='0.0.6')
-import org.svenehrke.dt.Invoker
-
-new Invoker().run(args)
