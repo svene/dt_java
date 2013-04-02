758a721afe1129d68652be4f60565b6f53e9b258
Sven Ehrke <sven.ehrke@canoo.com>
2013-04-02 14:27:32 +0200

    corrections to javafx template

--- templates/javafx/templatedirectory/src/main/java/@packagename@/Main.java
+++ templates/javafx/templatedirectory/src/main/java/@packagename@/Main.java
@@ -1,14 +1,20 @@
 package ${packagename};
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends Application {
-
-	private Label label;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -16,7 +22,6 @@ public class Main extends Application {
 
 	@Override
 	public void init() throws Exception {
-		label = new Label("Hello world");
 	}
 
 	@Override
@@ -24,11 +29,32 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage aStage) throws Exception {
-		StackPane root = new StackPane();
-		root.getChildren().add(label);
-		aStage.setTitle("Hello World");
-		aStage.setScene(new Scene(root, 200, 200));
-		aStage.show();
+	public void start(Stage stage) throws Exception {
+		final Group root = new Group();
+		Scene scene = new Scene(root, 500, 200, Color.DODGERBLUE);
+
+		final Text text = new Text(140, 120, "");
+		text.setFont(Font.font("Calibri", 35));
+		text.setFill(Color.WHITE);
+		text.setEffect(new DropShadow());        
+
+		Button bt = new Button("Show current time");     
+		bt.setLayoutX(180);
+		bt.setLayoutY(50);
+		bt.setOnAction(new EventHandler<ActionEvent>() 
+		{
+			@Override
+			public void handle(ActionEvent arg0)
+			{
+				text.setText("It is now: " + new SimpleDateFormat("hh:mm:ss").format(new Date()));
+			}
+		});
+
+		root.getChildren().add(bt);
+		root.getChildren().add(text);
+
+		stage.setTitle("HelloWorld in JavaFX 2.0");
+		stage.setScene(scene);
+		stage.show();
 	}
 }
